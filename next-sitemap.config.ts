76493c105ef157261633101d30418f3671bb7034
sajidhossain8272 <sajidhossain8272@gmail.com>
2025-03-14 06:25:37 +0600

    Sitemap not working

--- next-sitemap.config.ts
+++
@@ -1,8 +0,0 @@
-// next-sitemap.config.js
-/** @type {import('next-sitemap').IConfig} */
-module.exports = {
-  siteUrl: 'https://quick-convert-img.vercel.app/',
-  generateRobotsTxt: true,
-  // Optional: exclude specific paths if needed
-  // exclude: ['/admin/**'],
-};
